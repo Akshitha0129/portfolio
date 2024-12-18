69daa1fef2a175ab85fbc70ee523bba3c6045cb6
Ganesh2914 <Ganeshkrishnagoud@gmail.com>
2024-12-18 15:35:04 -0600

    MadeFewChanges

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [ 
-    "./**/*.html"
+    "./**/*.html", // This will include all HTML files in the project
+    "./css/**/*.css",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
 }
 
